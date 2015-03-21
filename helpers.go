6eb10bfa4e900a424f91c314f6efbcb0f9b03df2
Tom Adams <tom@holizz.com>
2015-03-21 10:52:31 -0400

    Scale images appropriately

--- helpers.go
+++ helpers.go
@@ -14,43 +14,47 @@ import (
 func listImages(w http.ResponseWriter, r *http.Request, images []Image) {
 	err := template.Must(template.New("").Parse(`<!doctype html>
 	<link rel="import" href="bower_components/core-pages/core-pages.html">
-	<link rel="import" href="bower_components/core-scaffold/core-scaffold.html">
 	<link rel="import" href="bower_components/core-menu/core-menu.html">
 	<link rel="import" href="bower_components/core-item/core-item.html">
 	<link rel="import" href="bower_components/core-image/core-image.html">
+	<link rel="import" href="bower_components/core-drawer-panel/core-drawer-panel.html">
+	<link rel="import" href="bower_components/core-header-panel/core-header-panel.html">
 
-	<body unresolved>
+	<body unresolved fullbleed>
 
-	<core-scaffold>
-
-		<core-header-panel navigation flex mode="seamed">
-			<core-toolbar>imagedb</core-toolbar>
+	<core-drawer-panel>
+		<core-header-panel drawer>
+			<core-toolbar id="navheader">
+				<span>Menu</span>
+			</core-toolbar>
 			<core-menu>
 				{{range .images}}
 					<core-item>
-						<core-image src="{{.RawLink}}" sizing="contain" style="width: 100px; height: 100px"></core-image>
+						<core-image src="{{.RawLink}}" preload sizing="contain" style="width: 100px; height: 100px"></core-image>
 					</core-item>
 				{{end}}
 			</core-menu>
 		</core-header-panel>
 
-		<div tool>
-			<form action="/search">
-				<input type="search" name="q" value="{{.q}}">
-				<input type="submit" value="Search">
-			</form>
-		</div>
+		<core-header-panel main>
+			<core-toolbar id="mainheader">
+				<form action="/search">
+					<input type="search" name="q" value="{{.q}}">
+					<input type="submit" value="Search">
+				</form>
+			</core-toolbar>
+			<core-pages>
+				{{range .images}}
+					<div>
+						<a href="{{.Link}}">
+							<core-image src="{{.RawLink}}" preload sizing="contain" style="width: 100%; height: 100%"></core-image>
+						</a>
+					</div>
+				{{end}}
+			</core-pages>
+		</core-header-panel>
 
-
-		<core-pages>
-			{{range .images}}
-				<div>
-					<a href="{{.Link}}"><core-image src="{{.RawLink}}" preload></core-image></a>
-				</div>
-			{{end}}
-		</core-pages>
-
-	</core-scaffold>
+	</core-drawer-panel>
 
 	<script>
 		var menu = document.querySelector('core-menu')
@@ -58,8 +62,17 @@ func listImages(w http.ResponseWriter, r *http.Request, images []Image) {
 
 		menu.addEventListener('core-activate', function() {
 			pages.selected = this.selected
+
+			// fix height
+			pages.style.height = (pages.parentElement.offsetHeight - pages.parentElement.firstElementChild.offsetHeight) + "px"
 		})
 	</script>
+
+	<style>
+	html /deep/ core-pages {
+		overflow: hidden;
+	}
+	</style>
 
 	</body>
 	`)).Execute(w, map[string]interface{}{
